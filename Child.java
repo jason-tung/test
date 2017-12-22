6582d7e5572fdf7f2693bda2fce6f2b1347690bc
jason <mickeu420@gmail.com>
2017-12-22 09:00:47 -0500

    finished parent and child classes

---
+++ Child.java
@@ -0,0 +1,24 @@
+public class Child extends Parent{
+    public static void main(String[] args){
+	Parent jerry = new Child();
+	Child dog = jerry;
+    }
+
+    /*
+    
+    public Child(){
+	name = "jerrryyyy";
+    }
+    
+    private int age;
+    private String name;
+
+    public int getAge(){
+	return age;
+    }
+
+    public void setAge(int a){
+	age = a;
+    }
+    */
+}
--- Parent.java
+++ Parent.java
@@ -1,1 +1,35 @@
-public static
+public class Parent{
+    private int age;
+    private String name;
+    
+    public Parent(int a, String n){
+	age = a;
+	name = n;
+    }
+    public Parent(int a){
+	Parent(a, "Jerry Ye");
+    }
+    public Parent(String n){
+	Parent (15, n);
+    }
+    public Parent(){
+	Parent(15, "Jerry Ye");
+    }
+
+    public String toString(){
+	return name + " age " + age;
+    }
+    public void setAge(int a){
+	age = a;
+    }
+    public void setName(String n){
+	name = n;
+    }
+    public int getAge(){
+	return age;
+    }
+    public String getName(){
+	return name;
+    }
+    
+}
